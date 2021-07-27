6196053236f4b683b808e7d750112f33d1916509
Joe Berry <jwberry@topshot.com>
2021-07-27 12:09:01 +0300

    Removed the jpeg md5 logic as it was too slow

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,6 @@
 #include "md5-a.h"
 #include "md5.h"
 
-extern "C" {
-    #include <jpeglib.h>
-}
 
 #define MD5_SIZE		16
 
@@ -16,47 +13,6 @@ int Compute_file_md5(const char *file_path, char *md5_str);
 
 using namespace std;
 
-std::string compute_md5_hash(const char* file_name) {
-    const int buffer_lines = 16;
-    struct jpeg_decompress_struct cinfo;
-    struct jpeg_error_mgr jerr;
-    JSAMPARRAY pJpegBuffer;
-    MD5 md5 = MD5();
-
-    // Open file.
-    FILE *infile = fopen(file_name, "rb");
-    if (infile == NULL) {
-        cerr << "Can't open file \"" << file_name  << "\"" << endl;
-        exit(1);
-    }
-
-    // Initialize decompression.
-    cinfo.err = jpeg_std_error(&jerr);
-    jpeg_create_decompress(&cinfo);
-    jpeg_stdio_src(&cinfo, infile);
-    jpeg_read_header(&cinfo, TRUE);
-    jpeg_start_decompress(&cinfo);
-    pJpegBuffer = (*cinfo.mem->alloc_sarray)((j_common_ptr) &cinfo, JPOOL_IMAGE, cinfo.output_width * cinfo.output_components, buffer_lines);
-
-    // Read JPEG.
-    while (cinfo.output_scanline < cinfo.output_height) {
-        int read_lines = jpeg_read_scanlines(&cinfo, pJpegBuffer, buffer_lines);
-
-        for (int y = 0; y < read_lines; y++) {
-            md5.update(pJpegBuffer[y], cinfo.output_components * cinfo.output_width);
-        }
-    }
-
-    // Clean-up.
-    fclose(infile);
-    jpeg_finish_decompress(&cinfo);
-    jpeg_destroy_decompress(&cinfo);
-
-    // Compute MD5.
-    md5.finalize();
-//    cout << md5.hexdigest() << endl;
-    return md5.hexdigest();
-}
 
 int main(int argc, char *argv[]) {
 
@@ -64,9 +20,6 @@ int main(int argc, char *argv[]) {
     std::string hashValue;
 
     const char* fileName1 = "data/LS7_3551.JPG";
-    md5Value = compute_md5_hash(fileName1);
-    cout << "md5 result is " << md5Value << endl;
-
 	char md5_value1[MD5_SIZE*2];
     int errCode = Compute_file_md5(fileName1, md5_value1);
     if (0 != errCode) {
@@ -76,9 +29,6 @@ int main(int argc, char *argv[]) {
 
 
     const char* fileName2 = "data/LS7_3552.JPG";
-    md5Value = compute_md5_hash(fileName2);
-    cout << "md5 result is " << md5Value << endl;
-
 	char md5_value2[MD5_SIZE];
     errCode = Compute_file_md5(fileName2, md5_value2);
     if (0 != errCode) {
